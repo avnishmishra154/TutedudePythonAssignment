a78020664744b12c8fe3e76e30debe01b29fb4bf
Avnish Mishra <avnishmishra154@gmail.com>
2025-11-12 21:27:35 +0530

    Add files via upload

---
+++ Assignment 3/task2.py
@@ -0,0 +1,11 @@
+import math
+
+num = float(input("Enter a number: "))
+
+sqrt_num = math.sqrt(num)
+log_num = math.log(num)
+sine_num = math.sin(num)
+
+print(f"Square root of {num} = {sqrt_num}")
+print(f"Natural logarithm of {num} = {log_num}")
+print(f"Sine of {num} (in radians) = {sine_num}")
